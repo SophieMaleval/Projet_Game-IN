0be9168c72596ca8da0518941820d0e2852867c6
athegreg <athisthierot@gmail.com>
2021-04-25 17:30:43 +0200

    Système de dialogue premier test + expression

---
+++ Projet_GAMEIN/Assets/Assets-Athenais/Scripts/PlayerController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header ("Visual")]
+    public Sprite NorthSprite ;
+    public Sprite EastSprite ;
+    public Sprite SouthSprite ;
+    public Sprite WestSprite ;
+
+
+    Direction CurrentDirection ;
+    Vector2 InputPlayer ;
+    bool IsMoving = false ;
+    Vector3 StartPos ;
+    Vector3 Endpos ;
+    float T ;
+
+    [Header ("Walk Setting")]
+    public float WalkSpeed = 3f ;
+    public bool canMove;
+
+    void Update() 
+    {
+
+        if(!canMove){
+
+            return;
+
+        }
+
+        if(!IsMoving)
+        {
+            InputPlayer = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if(Mathf.Abs(InputPlayer.x) > Mathf.Abs(InputPlayer.y))
+                InputPlayer.y = 0 ;
+            else
+                InputPlayer.x = 0;
+
+            if(InputPlayer != Vector2.zero)
+            {
+                if(InputPlayer.x < 0)
+                {
+                    CurrentDirection = Direction.West ;
+                }
+                if(InputPlayer.x > 0)
+                {
+                    CurrentDirection = Direction.East ;
+                }
+                if(InputPlayer.y < 0)
+                {
+                    CurrentDirection = Direction.South ;
+                }
+                if(InputPlayer.y > 0)
+                {
+                    CurrentDirection = Direction.North ;
+                }
+
+                switch(CurrentDirection)
+                {
+                    case Direction.North:
+                        //gameObject.GetComponent<SpriteRenderer>().sprite = NorthSprite ;
+                        gameObject.transform.localEulerAngles = new Vector3(0,0,0f);
+                        break ;
+                    case Direction.East:
+                        //gameObject.GetComponent<SpriteRenderer>().sprite = EastSprite ;
+                        gameObject.transform.localEulerAngles = new Vector3(0,0,-90f);
+                        break ;
+                    case Direction.South:
+                        //gameObject.GetComponent<SpriteRenderer>().sprite = SouthSprite ;
+                        gameObject.transform.localEulerAngles = new Vector3(0,0,180f);
+                        break ;
+                    case Direction.West:
+                        //gameObject.GetComponent<SpriteRenderer>().sprite = WestSprite ;
+                        gameObject.transform.localEulerAngles = new Vector3(0,0,90f);
+                        break ;
+                }
+
+                StartCoroutine(Move(transform));
+            }
+        }
+    }
+
+    public IEnumerator Move(Transform Entity)
+    {
+        IsMoving = true ;
+        StartPos = Entity.position ;
+        T = 0 ;
+
+        Endpos = new Vector3(StartPos.x + System.Math.Sign(InputPlayer.x), StartPos.y + System.Math.Sign(InputPlayer.y), StartPos.z);
+
+        while(T < 1f)
+        {
+            T += Time.deltaTime * WalkSpeed ;
+            Entity.position = Vector3.Lerp(StartPos, Endpos, T);
+            yield return null ;
+        }
+
+
+        IsMoving = false ;
+        yield return 0 ;
+    }
+
+
+    enum Direction
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+
+
+}
+
+
+
---
+++ Projet_GAMEIN/Assets/Assets-Athenais/Scripts/TextBoxManager.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+    public GameObject textBox;
+
+    public Text theText;
+
+    public TextAsset thetext;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+    public bool isActive;
+
+    public bool stopPlayerMovement;
+
+    public PlayerController player;
+	public string theVoice;
+	public float speedtalk;
+	
+	public bool waitendtxt = true;
+	
+	private int j;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        player = FindObjectOfType<PlayerController>();
+
+        if (thetext != null)
+        {
+            textLines = (thetext.text.Split('\n'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+
+
+        if (isActive)
+        {
+            EnableTextBox();
+        }
+        else
+        {
+            DisableTextBox();
+        }
+    }
+
+    void Update()
+    {
+        if (!isActive)
+        {
+            return;
+        }
+
+
+        //theText.text = textLines[currentLine];
+		
+
+        if (Input.GetKeyDown(KeyCode.Space) && waitendtxt == true)
+        {
+			StartCoroutine(AnimateText());
+            currentLine += 1;
+
+        }
+
+        if(currentLine == endAtLine)
+        {
+            DisableTextBox();
+        }
+    }
+
+
+    public void EnableTextBox()
+    {
+		StartCoroutine(AnimateText());
+        textBox.SetActive(true);
+        isActive = true;
+
+        if (stopPlayerMovement)
+        {
+            player.canMove = false;
+        }
+
+    }
+
+
+    public void DisableTextBox()
+    {
+        textBox.SetActive(false);
+        isActive = false;
+
+        player.canMove = true;
+
+    }
+
+    public void ReloadScript(TextAsset theText)
+    {
+        if(theText != null)
+        {
+            textLines = new string[1];
+            textLines = (theText.text.Split('\n'));
+
+        }
+
+    }
+	
+	IEnumerator AnimateText(){
+     
+     for (int i = 0; i < (textLines[currentLine].Length+1); i++)
+     {
+		 j = j + 1;
+		 if (j >= (textLines[currentLine].Length)){
+			j = 0; 
+		 }
+		 
+         theText.text = textLines[currentLine].Substring(0, i);
+		 
+		 if(textLines[currentLine][j] != ' '){
+			FindObjectOfType<AudioManager>().Play(theVoice);
+		 }
+		 if (i == textLines[currentLine].Length){
+			 waitendtxt = true;
+		 }
+		 else
+		 {
+			waitendtxt = false; 
+		 }
+         yield return new WaitForSeconds(speedtalk);
+     }
+	 j = -1;
+
+	}
+	
+}
