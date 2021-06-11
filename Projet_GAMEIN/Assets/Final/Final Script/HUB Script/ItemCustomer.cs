f264c0a10664f1eb806a98a8e82c80475ec9cbea
Antonin.M <antonin-08m@hotmail.fr>
2021-06-11 21:54:34 +0200

    Remise en place ancien sprite custom

--- Projet_GAMEIN/Assets/Final/Final Script/HUB Script/ItemCustomer.cs
+++ Projet_GAMEIN/Assets/Final/Final Script/HUB Script/ItemCustomer.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 [CreateAssetMenu (menuName = "Item - Custome Character")]
 public class ItemCustomer : ScriptableObject
 {
+    [Header ("Customisation de Personnage")]
     public Sprite DisplayCustomisation ;
     public Sprite Face ;
     public Sprite Profil ;
     public Sprite Dos ;
+
+    [Header ("Animation In Game")]
+    public AnimatorControllerParameter Idle ;
 }
