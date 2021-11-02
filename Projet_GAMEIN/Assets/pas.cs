9ff446d093e38700914fe59429ad78ee1ff257a7
athegreg <athisthierot@gmail.com>
2021-11-02 10:17:57 +0100

    ?

---
+++ Projet_GAMEIN/Assets/pas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pas : MonoBehaviour
+{
+    public AudioSource Paspied;
+    public AudioSource Paspied2;
+    // Start is called before the first frame update
+   public void Pas(){
+
+       Paspied.Play();
+   }
+
+      public void Pas2(){
+
+       Paspied2.Play();
+   }
+}
