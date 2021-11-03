00cb216c7f7f067a8a4e6b48d1f171d61a7455c0
Antonin.M <antonin-08m@hotmail.fr>
2021-11-03 10:30:55 +0100

    Set Up WebGL Export

---
+++ Projet_GAMEIN/Assets/Folder Perso/Anto/LoadingScreenScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreenScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitAndLoadMenu());
+    }
+
+    IEnumerator WaitAndLoadMenu()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("Menu");
+    }
+}
