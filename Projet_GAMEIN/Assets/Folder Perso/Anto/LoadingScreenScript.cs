ebaa3fcc1e45bfa5651acfca562ef2ccc9d0b40f
Antonin.M <antonin-08m@hotmail.fr>
2021-11-03 16:41:47 +0100

    Loading Screen 1st Set Up

--- Projet_GAMEIN/Assets/Folder Perso/Anto/LoadingScreenScript.cs
+++ Projet_GAMEIN/Assets/Folder Perso/Anto/LoadingScreenScript.cs
@@ -2,19 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI ;
+using DG.Tweening;
 
 public class LoadingScreenScript : MonoBehaviour
 {
+    [SerializeField] private Image Fade ;
+    [SerializeField] private float TimeWait = 5f ;
 
-    // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(WaitAndLoadMenu());
+        StartCoroutine(FadeAnimtion(false));
+        StartCoroutine(WaitAndLoadMenu(TimeWait));
     }
 
-    IEnumerator WaitAndLoadMenu()
+    IEnumerator WaitAndLoadMenu(float TimeToWait)
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(TimeToWait - 1.25f);
+        StartCoroutine(FadeAnimtion(true));
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene("Menu");
+    }
+
+    IEnumerator FadeAnimtion(bool GameAsLunch)
+    {
+        if(GameAsLunch == false)
+        {
+            yield return new WaitForSeconds(1f);
+            Fade.DOFade(0, 1f) ;
+            yield return new WaitForSeconds(0.5f);
+            Fade.raycastTarget = false ;
+        }
+
+        if(GameAsLunch == true)
+        {
+            Fade.raycastTarget = true ;
+            Fade.DOFade(1, 1f) ;
+            yield return new WaitForSeconds(2f);
+            SceneManager.LoadScene("Character Customer") ;
+        }
     }
 }
