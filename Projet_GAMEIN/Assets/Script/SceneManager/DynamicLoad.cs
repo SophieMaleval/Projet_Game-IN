464e86e2691e4ee1949ac3c7e52440937fc5d51e
Binhicus <abdoulaye-ba@live.fr>
2021-11-11 23:44:52 +0100

    chargement dynamique v1

---
+++ Projet_GAMEIN/Assets/Script/SceneManager/DynamicLoad.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum CheckMethod
+{
+    Distance, 
+    Trigger
+}
+public class DynamicLoad : MonoBehaviour
+{
+    public Transform player;
+    public CheckMethod checkMethod;
+    public float loadRange;
+
+    //Scene state
+    bool isLoaded; //eviter de charger 2x
+    bool shouldLoad; //pour la méthode en trigger
+    void Start()
+    {
+        
+    }
+    void LoadScene()
+    {
+        if (!isLoaded)
+        {
+            SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive); // le nom du game object "Part n+1" doit être identique à celui de la scène à charger
+            isLoaded = true;
+        }
+    }
+
+    void UnloadScene()
+    {
+        if (isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(gameObject.name);
+            isLoaded = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (checkMethod == CheckMethod.Distance)
+        {
+            DistanceCheck();
+        }
+        else if (checkMethod == CheckMethod.Trigger)
+        {
+            TriggerCheck();
+        }
+    }
+//method Distance
+    void DistanceCheck()
+    {
+        if(Vector3.Distance(player.position, transform.position) < loadRange)
+        {
+            LoadScene();
+        }
+        else
+        {
+            UnloadScene();
+        }
+    }
+
+  
+
+ //method Trigger
+    void TriggerCheck()
+    {
+        if (shouldLoad)
+        {
+            LoadScene();
+        }
+        else
+        {
+            UnloadScene();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            shouldLoad = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            shouldLoad = false;
+        }
+    }
+}
