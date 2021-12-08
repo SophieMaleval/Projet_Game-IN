15b7e7d83e4bc3fae340c7a8ae0456f34c11f263
Binhicus <abdoulaye-ba@live.fr>
2021-12-08 10:38:03 +0100

    maj_questSys
    intitulé

--- Projet_GAMEIN/Assets/Script/Quest/QuestCt.cs
+++ Projet_GAMEIN/Assets/Script/Quest/QuestCt.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [System.Serializable]
 public class QuestCt
 {
+    //[HideInInspector]
+    public string intitule;
     public int questCode;
     [SerializeField] [TextArea] public string questTitle;   
     [TextArea(3, 7)] public string[] questGoal;   
