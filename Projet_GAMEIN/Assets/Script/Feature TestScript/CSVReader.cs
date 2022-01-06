59e3903af32770efe206bfc005d5681701313d96
NioZebra <antonin.mercier@e-artsup.net>
2022-01-06 16:05:37 +0100

    Preparation Traduction Quête

--- Projet_GAMEIN/Assets/Script/Feature TestScript/CSVReader.cs
+++ Projet_GAMEIN/Assets/Script/Feature TestScript/CSVReader.cs
@@ -87,60 +87,57 @@ public class CSVReader : MonoBehaviour
     void ReaderDialogCSV(TextAsset DialogDataLanguage, List<DialogueContainer> TargetList) 
     {
         TargetList.Clear();
-        string[] LineData = DialogDataLanguage.text.Split(new string[] { "\n" }, StringSplitOptions.None); // Data correspond à chaque Ligne
+        string[] LineData = DialogDataLanguage.text.Split(new string[] { "\n" }, StringSplitOptions.None); // LineData correspond à chaque Ligne
 
-        for (int LD = 0; LD < LineData.Length; LD++)
-        {
-            if(LD != 0)
-            {              
-                string[] Data = LineData[LD].Split(new string[] { ";" }, StringSplitOptions.None); // Data correspond à chaque case    
+        for (int LD = 1; LD < LineData.Length; LD++)
+        {         
+            string[] Data = LineData[LD].Split(new string[] { ";" }, StringSplitOptions.None); // Data correspond à chaque case    
 
-                if(Data[0] != "")
-                {
-                    DialogueContainer InfoDiag = new DialogueContainer();
-                    InfoDiag.Entreprise = ReplaceCharacter(Data[0]);
-                    InfoDiag.Name = ReplaceCharacter(Data[1]);
-                    InfoDiag.OpeningDialogue = ReplaceCharacter(Data[2]);
-                    InfoDiag.CloseDiscussion = ReplaceCharacter(Data[3]);
+            if(Data[0] != "")
+            {
+                DialogueContainer InfoDiag = new DialogueContainer();
+                InfoDiag.Entreprise = ReplaceCharacter(Data[0]);
+                InfoDiag.Name = ReplaceCharacter(Data[1]);
+                InfoDiag.OpeningDialogue = ReplaceCharacter(Data[2]);
+                InfoDiag.CloseDiscussion = ReplaceCharacter(Data[3]);
                     
-                    InfoDiag.Question1 = ReplaceCharacter(Data[4]);
-                    InfoDiag.Question2 = ReplaceCharacter(Data[5]);
-                    InfoDiag.Question3 = ReplaceCharacter(Data[6]);
-                    InfoDiag.Question4 = ReplaceCharacter(Data[7]);
-                    InfoDiag.Question5 = ReplaceCharacter(Data[8]);
-                    InfoDiag.Question6 = ReplaceCharacter(Data[9]);
-                    InfoDiag.Question7 = ReplaceCharacter(Data[10]);
-                    InfoDiag.Question8 = ReplaceCharacter(Data[11]);
-                    InfoDiag.Question9 = ReplaceCharacter(Data[12]);
-                    InfoDiag.Question10 = ReplaceCharacter(Data[13]);
-                    InfoDiag.Aurevoir = ReplaceCharacter(Data[14]);
+                InfoDiag.Question1 = ReplaceCharacter(Data[4]);
+                InfoDiag.Question2 = ReplaceCharacter(Data[5]);
+                InfoDiag.Question3 = ReplaceCharacter(Data[6]);
+                InfoDiag.Question4 = ReplaceCharacter(Data[7]);
+                InfoDiag.Question5 = ReplaceCharacter(Data[8]);
+                InfoDiag.Question6 = ReplaceCharacter(Data[9]);
+                InfoDiag.Question7 = ReplaceCharacter(Data[10]);
+                InfoDiag.Question8 = ReplaceCharacter(Data[11]);
+                InfoDiag.Question9 = ReplaceCharacter(Data[12]);
+                InfoDiag.Question10 = ReplaceCharacter(Data[13]);
+                InfoDiag.Aurevoir = ReplaceCharacter(Data[14]);
 
                     
 
-                    InfoDiag.Dialogue1 = ReplaceCharacter(Data[15]);
-                    InfoDiag.Dialogue2 = ReplaceCharacter(Data[16]);
-                    InfoDiag.Dialogue3 = ReplaceCharacter(Data[17]);
-                    InfoDiag.Dialogue4 = ReplaceCharacter(Data[18]);
-                    InfoDiag.Dialogue5 = ReplaceCharacter(Data[19]);
-                    InfoDiag.Dialogue6 = ReplaceCharacter(Data[20]);
-                    InfoDiag.Dialogue7 = ReplaceCharacter(Data[21]);
-                    InfoDiag.Dialogue8 = ReplaceCharacter(Data[22]);
-                    InfoDiag.Dialogue9 = ReplaceCharacter(Data[23]);    
-                    InfoDiag.Dialogue10 = ReplaceCharacter(Data[24]);    
-                    InfoDiag.Dialogue11 = ReplaceCharacter(Data[25]);    
-                    InfoDiag.Dialogue12 = ReplaceCharacter(Data[26]);    
-                    InfoDiag.Dialogue13 = ReplaceCharacter(Data[27]);    
-                    InfoDiag.Dialogue14 = ReplaceCharacter(Data[28]);    
-                    InfoDiag.Dialogue15 = ReplaceCharacter(Data[29]);    
-                    InfoDiag.Dialogue16 = ReplaceCharacter(Data[30]);    
-                    InfoDiag.Dialogue17 = ReplaceCharacter(Data[31]);    
-                    InfoDiag.Dialogue18 = ReplaceCharacter(Data[32]);    
-                    InfoDiag.Dialogue19 = ReplaceCharacter(Data[33]);    
-                    InfoDiag.Dialogue20 = ReplaceCharacter(Data[34]);    
+                InfoDiag.Dialogue1 = ReplaceCharacter(Data[15]);
+                InfoDiag.Dialogue2 = ReplaceCharacter(Data[16]);
+                InfoDiag.Dialogue3 = ReplaceCharacter(Data[17]);
+                InfoDiag.Dialogue4 = ReplaceCharacter(Data[18]);
+                InfoDiag.Dialogue5 = ReplaceCharacter(Data[19]);
+                InfoDiag.Dialogue6 = ReplaceCharacter(Data[20]);
+                InfoDiag.Dialogue7 = ReplaceCharacter(Data[21]);
+                InfoDiag.Dialogue8 = ReplaceCharacter(Data[22]);
+                InfoDiag.Dialogue9 = ReplaceCharacter(Data[23]);    
+                InfoDiag.Dialogue10 = ReplaceCharacter(Data[24]);    
+                InfoDiag.Dialogue11 = ReplaceCharacter(Data[25]);    
+                InfoDiag.Dialogue12 = ReplaceCharacter(Data[26]);    
+                InfoDiag.Dialogue13 = ReplaceCharacter(Data[27]);    
+                InfoDiag.Dialogue14 = ReplaceCharacter(Data[28]);    
+                InfoDiag.Dialogue15 = ReplaceCharacter(Data[29]);    
+                InfoDiag.Dialogue16 = ReplaceCharacter(Data[30]);    
+                InfoDiag.Dialogue17 = ReplaceCharacter(Data[31]);    
+                InfoDiag.Dialogue18 = ReplaceCharacter(Data[32]);    
+                InfoDiag.Dialogue19 = ReplaceCharacter(Data[33]);    
+                InfoDiag.Dialogue20 = ReplaceCharacter(Data[34]);    
 
 
-                    TargetList.Add(InfoDiag);
-                }
+                TargetList.Add(InfoDiag);
             }
         }
     }
@@ -161,7 +158,30 @@ public class CSVReader : MonoBehaviour
 
     void ReadQuestCSV()
     {
+        string[] LineData = QuestText.text.Split(new string[] { "\n" }, StringSplitOptions.None) ; //Line Data correspond à chaque Ligne
+        int QuestCode = 0;
+        for (int LD = 1; LD < LineData.Length; LD++)
+        {
+            string[] Data = LineData[LD].Split(new string[] { ";" }, StringSplitOptions.None) ; //Data correspond à chaque Case 
 
+            if(Data[0] != "")
+            {
+                QuestCt NewQuestCt = new QuestCt() ;
+                NewQuestCt.questCode = QuestCode ;
+                string[] QuestGoal ;
+
+                for (int i = 0; i < Data.Length; i++)
+                {
+                    if(i == 0) NewQuestCt.intitule = Data[0] ; 
+                    if(i == 1) NewQuestCt.questTitle = Data[1] ; 
+
+                    if(i > 1) NewQuestCt.questGoal.
+                }
+
+            } else {
+                QuestCode ++ ;
+            } 
+        }
     }
 
 
