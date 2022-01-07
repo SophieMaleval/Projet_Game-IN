7a143014d9df64aad71ec0e5623a08940d7776d1
athegreg <athisthierot@gmail.com>
2022-01-07 18:25:14 +0100

    escaliers

--- Projet_GAMEIN/Assets/Script/CraftersController.cs
+++ Projet_GAMEIN/Assets/Script/CraftersController.cs
@@ -7,6 +7,8 @@ public class CraftersController : MonoBehaviour
     public GameObject Etage1;
     public GameObject Etage0;
     public bool IsUp = false;
+    // public GameObject CollisionToUp;
+    // public GameObject CollisionToDown;
 
      void Awake() {
          IsUp = false;
@@ -15,6 +17,8 @@ public class CraftersController : MonoBehaviour
     private void Update() {
         if(IsUp == true)
         {
+            // CollisionToUp.SetActive(false);
+            // CollisionToDown.SetActive(true);
             //Debug.Log("en haut");
             Etage0.SetActive(false);
             Etage1.SetActive(true);
@@ -22,14 +26,15 @@ public class CraftersController : MonoBehaviour
         }
          if(IsUp == false)
         {
-          
+            // CollisionToUp.SetActive(true);
+            // CollisionToDown.SetActive(false);
             //Debug.Log("en bas");
             Etage0.SetActive(true);
             Etage1.SetActive(false);
         
         }
     }
-    private void OnTriggerExit2D(Collider2D other) 
+    private void OnTriggerEnter2D(Collider2D other) 
     {
         
           if(other.gameObject.tag == "Player")
@@ -44,10 +49,14 @@ public class CraftersController : MonoBehaviour
 
         if(StairsTaken == false){
                   Debug.Log(" on prend l'escalier vers le haut");
+                  this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                  this.gameObject.GetComponent<CircleCollider2D>().enabled = true;
                   IsUp =  true;
               }
         if(StairsTaken == true){
                   Debug.Log(" on prend l'escalier vers le bas");
+                  this.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+                  this.gameObject.GetComponent<CircleCollider2D>().enabled = false;
                   IsUp = false;
     }
 
