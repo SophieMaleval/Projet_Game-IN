936cdbb5515840a7f1dd2ce1613b4c2ff4de6923
NioZebra <antonin.mercier@e-artsup.net>
2022-03-17 18:39:12 +0100

    Preparation Lever

---
+++ Projet_GAMEIN/Assets/Prefab/Interactible/Obstacle Monde/LeverController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverController : MonoBehaviour
+{
+    [Header ("Lever Gestion")]
+    [SerializeField] private List<SpriteRenderer> LeverRender = new List<SpriteRenderer>();
+    private bool LeverIsActivate = false ;
+    [SerializeField] private Sprite LeverDisable ;
+    [SerializeField] private Sprite LeverDisableHighlight ;
+    [SerializeField] private Sprite LeverEnable ;
+    [SerializeField] private Sprite LeverEnableHighlight ;
+    [Space]
+    [SerializeField] private List<WorldObstacle> ObstacleInitiallyDisable = new List<WorldObstacle>() ;
+    [SerializeField] private List<WorldObstacle> ObstacleInitiallyEnable = new List<WorldObstacle>() ;
+
+
+    private PlayerScript Player ;
+
+    private void Awake() {
+        if(GameObject.Find("Player") != null)   // Récupère le player au lancement de la scène
+        {    Player = GameObject.Find("Player").GetComponent<PlayerScript>() ; }
+    }
+
+    void ActivationLever()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == ("Player"))
+        {
+            
+            Player.SwitchInputSprite();
+        }      
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == ("Player"))
+        {
+
+            Player.SwitchInputSprite();
+        }
+    }
+
+    void ShowHighlightLever(bool StateHighlight)
+    {
+        for (int Sp = 0; Sp < LeverRender.Count; Sp++)
+        {
+            if(!LeverIsActivate)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Projet_GAMEIN/Assets/Prefab/Interactible/Obstacle Monde/WorldObstacle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WorldObstacle : MonoBehaviour
+{
+    private SpriteRenderer SpriteRend ;
+
+    [SerializeField] private Sprite DisableSprite ;
+    [SerializeField] private Sprite EnableSprite ;
+
+    [SerializeField] private Collider2D PassageDisableColl ;
+    [SerializeField] private Collider2D PassageEnableColl ;
+
+    private void Awake() {
+        SpriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    void SetPassage(bool State)
+    {
+        if(!State)
+        {
+            SpriteRend.sprite = DisableSprite ;
+            PassageDisableColl.enabled = true ;
+            PassageEnableColl.enabled = false ;
+        } else {
+            SpriteRend.sprite = EnableSprite ;
+            PassageDisableColl.enabled = false ;
+            PassageEnableColl.enabled = true ;
+        }
+    }
+}
