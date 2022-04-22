ae743cb03c4992c315b28b1728ec27f4bb9a8a58
Binhicus <abdoulaye-ba@live.fr>
2022-04-22 19:38:33 +0200

    rectif questSys
    quand on finit une quête on revient sur la quête 0

--- Projet_GAMEIN/Assets/Script/Quest/QuestSys.cs
+++ Projet_GAMEIN/Assets/Script/Quest/QuestSys.cs
@@ -120,12 +120,12 @@ public class QuestSys : MonoBehaviour
 
     public void NextLevel()
     {
-        niveau++;
+        /*niveau++;
         etape = 0;
         if(niveau > sizeOfList)
-        {
+        {*/
             Roaming();
-        }
+        //}
     }
     public void Roaming()
     {
