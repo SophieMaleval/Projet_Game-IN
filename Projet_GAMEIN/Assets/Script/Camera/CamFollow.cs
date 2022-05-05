1749a1890e99f43cfb08e24b41cb8b7bd2aa7132
Binhicus <abdoulaye-ba@live.fr>
2022-05-05 11:35:03 +0200

    minimap finito pipo

---
+++ Projet_GAMEIN/Assets/Script/Camera/CamFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public Transform player;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.position = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+    }
+}
