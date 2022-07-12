74a5135f3614fd48bcee5298983c8238aa1dce56
Allosius27 <yanis.queroix@orange.fr>
2022-07-12 14:36:05 +0200

    [Prog] Update UI Prefabs

---
+++ Projet_GAMEIN/Assets/Script/UI/ButtonCtrl.cs
@@ -0,0 +1,61 @@
+﻿using AllosiusDev.Audio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonCtrl : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+{
+    #region Properties
+
+    public Button button { get; protected set; }
+
+    public AudioData SfxButtonHighlightedObject => sfxButtonHighlightedObject;
+    public AudioData SfxButtonClickedObject => sfxButtonClickedObject;
+
+    #endregion
+
+    #region UnityInspector
+
+    [SerializeField] private AudioData sfxButtonHighlightedObject;
+    [SerializeField] private AudioData sfxButtonClickedObject;
+
+    #endregion
+
+    #region Behaviour
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Mouse enter");
+        AudioController.Instance.PlayAudio(sfxButtonHighlightedObject);
+    }
+
+    public virtual void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Mouse Down");
+        AudioController.Instance.PlayAudio(sfxButtonClickedObject);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("Mouse Up");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Mouse exit");
+    }
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    public virtual void Start()
+    {
+
+    }
+
+    #endregion
+}
